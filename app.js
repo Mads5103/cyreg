9f5c75808da7cebaba0b1a774c4b4839edfdfcc0
NGeertinger <s153140@student.dtu.dk>
2017-06-08 12:39:38 +0200

    TjekData -> TjekData

--- app.js
+++ app.js
@@ -158,7 +158,8 @@ app.post('/TjekData', urlencodedParser, function (req, res) {
     logdata4.antal_optagede_pladser = db2.getData("/data" + req.body.check_vogn).antal_optagede_pladser;
     logdata4.antal_pladser = db2.getData("/data" + req.body.check_vogn).antal_pladser;
     logdata4.pladsstring = db2.getData("/data" + req.body.check_vogn).pladsstring;
-    res.send(logdata4);
+    res.sendFile( __dirname + "/" + "TjekData.html" );
+    //res.send(logdata4);
     //if(logdata4.antal_pladser -
     //res.write(logdata4);
     //res.end();
